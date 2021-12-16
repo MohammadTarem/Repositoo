e7eb0fdce12d5ecdcfef504ab320636c337081fd
Mohammad Tarem <mohammad.tarem@live.com>
2021-12-16 16:47:29 +0330

    Changes Id to TId

--- Repositoo/IDatabaseOperations.cs
+++ Repositoo/IDatabaseOperations.cs
@@ -5,12 +5,12 @@ using System.Threading.Tasks;
 
 namespace Repositoo
 {
-    public interface IDatabaseOperations<Id, TEntity> : IQueryable<TEntity>, IDisposable
+    public interface IDatabaseOperations<TId, TEntity> : IQueryable<TEntity>, IDisposable
     {
 
-        public Task<Id> AddAsync(TEntity item, CancellationToken cancllationToken = default);
-        public Task<Id> UpdateAsync(TEntity item, CancellationToken cancellationToken = default);
-        public Task<Id> DeleteAsync(TEntity key, CancellationToken cancellationToken = default);
+        public Task<TId> AddAsync(TEntity item, CancellationToken cancllationToken = default);
+        public Task<TId> UpdateAsync(TEntity item, CancellationToken cancellationToken = default);
+        public Task<TId> DeleteAsync(TEntity key, CancellationToken cancellationToken = default);
 
     }
 
