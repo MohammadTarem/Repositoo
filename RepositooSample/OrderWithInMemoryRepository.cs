10d2dd1022420d4f92c9c4ab631acf4a50cd950e
Mohammad Tarem <mohammad.tarem@live.com>
2022-02-17 11:08:51 +0330

    Adds InMemory repository class

---
+++ RepositooSample/OrderWithInMemoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Repositoo;
+namespace RepositooSample
+{
+    public class OrderWithInMemoryRepository : BaseRepository<int, Order>
+    {
+        public OrderWithInMemoryRepository(InMemoryOperations<int, Order> operations) : base(operations)
+        {
+
+        }
+    }
+}
